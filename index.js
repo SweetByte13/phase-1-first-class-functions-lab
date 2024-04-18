2f4f502a1c4ecb6906626b4dd54cae04e76bd48a
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-04-18 11:42:41 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+let array = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = (array) => {
+ return array.slice(0, 2)
+};
+
+const returnLastTwoDrivers = (array) => {
+    return array.slice (2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (factor) => {
+    return function (fare) {
+        return fare * factor
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+let selectDifferentDrivers = (array, driverSelection) => {
+return driverSelection(array);
+}
+
+
+
